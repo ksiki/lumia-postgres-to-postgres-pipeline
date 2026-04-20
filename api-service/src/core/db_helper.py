551a9effd26ade88aad7d5bf193c26c0e81b8d43
ksiki <j.kolyasha@gmail.com>
2026-04-20 23:56:25 +0300

    database halper

---
+++ api-service/src/core/db_helper.py
@@ -0,0 +1,30 @@
+from typing import Final
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    AsyncSession, 
+    create_async_engine, 
+    async_sessionmaker, 
+    async_scoped_session 
+)
+
+from src.core.config import SETTINGS
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.__engine = create_async_engine(url=url, echo=echo)
+        self.session_factory = async_sessionmaker(
+            bind=self.__engine,
+            autoflush=False,
+            expire_on_commit=False,
+        )
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+
+
+DATABASE_HELPER: Final[DatabaseHelper] = DatabaseHelper(
+    url=SETTINGS.db_url,
+    echo=SETTINGS.debug
+)
